c024ae486098d0fab9d875f09520ac2463ca4f7f
feroz5 <SFerozMohamedMeeraSahib@dal.ca>
2018-02-08 17:40:06 -0500

    added

---
+++ zip.py
@@ -0,0 +1,17 @@
+numberList = [1, 2, 3]
+strList = ['one', 'two', 'three']
+
+
+result = zip()
+
+
+resultList = list(result)
+print(resultList)
+
+
+result = zip(numberList, strList)
+
+
+resultSet = set(result)
+print(resultSet)
+
